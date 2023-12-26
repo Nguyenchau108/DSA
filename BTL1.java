d6a7deea1f33433d8f88f398e1adede7826513dd
Nguyenchau108 <153355324+Nguyenchau108@users.noreply.github.com>
2023-12-26 14:34:42 +0700

    Add files via upload

---
+++ BTL1.java
@@ -0,0 +1,20 @@
+public class BTL1 {
+    public static void main(String[] args){
+        //Bai 1
+        System.out.println("Hello World");
+        //Bai 2
+        String a = "Hello World";
+        //Bai 3
+        System.out.println(a);
+        String b = "Hello";
+        String c = "World";
+        System.out.println(b + " " + c);
+        //Bai 4
+        Myprint(b, c);
+    }
+    static void Myprint(String b, String c){
+       
+        System.out.println(b + " " + c);
+    }
+    
+}
